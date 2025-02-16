731a0ddc0bb41ce3b47597408eee347cd3bff763
shravya <gshravyagoud05@gmail.com>
2025-02-16 12:06:37 -0500

    Chargebacks processor resource update

--- src/main/java/StripeParsers/Dispute.java
+++ src/main/java/StripeParsers/Dispute.java
@@ -1,6 +1,7 @@
 package StripeParsers;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Dispute {
 
@@ -21,5 +22,24 @@ public class Dispute {
     @Override
     public String toString(){
         return transactionId + " | " + network + " | " + reason + " | " + amount + " | " + date;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if(this==o) return true;
+        if(o ==null || getClass() !=o.getClass()) return false;
+
+        Dispute dispute = (Dispute) o;
+        return Objects.equals(transactionId, dispute.transactionId) &&
+                Objects.equals(network, dispute.network) &&
+                Objects.equals(reason, dispute.reason) &&
+                Objects.equals(date, dispute.date) &&
+                Objects.equals(amount, dispute.amount);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(transactionId, network, reason, date, amount);
     }
 }
